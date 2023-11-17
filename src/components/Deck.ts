21e3d7ec56d66994e4e29fe5e2dc50946d09323e
Kyo Terada <sitiang120@gmail.com>
2023-11-17 12:26:23 +0900

    ♻️ デッキクラスのメソッドを大きく変更した

--- src/components/Deck.ts
+++ src/components/Deck.ts
@@ -1,54 +1,51 @@
-import { Card } from "@/components/Card"
-import { SUITS, RANKS, GAMESWITHJOKER } from "@/constants/constants"
+  import { Card } from "@/components/Card"
+  import { SUITS, RANKS, GAMESWITHJOKER, GAMETYPE } from "@/constants/constants"
 
-export class Deck {
-  private gameType: string
-  private deck: Card[]
+  export class Deck {
+    private gameType: GAMETYPE
+    private deck: Card[]
 
-  constructor(gameType: string) {
-    this.gameType = gameType.toLowerCase()
-    this.deck = Deck.generateDeck(GAMESWITHJOKER.includes(this.gameType))
-  }
+    constructor(gameType: GAMETYPE) {
+      this.gameType = gameType
+      this.deck = Deck.generateDeck(GAMESWITHJOKER.includes(this.gameType))
+    }
 
-  // デッキ作成 ジョーカー追加 シャッフル
-  public static generateDeck(
-    addJoker: boolean = false,
-    shuffle: boolean = true
-  ): Card[] {
-    const newDeck: Card[] = []
+    // デッキ作成 ジョーカー追加
+    public static generateDeck(addJoker: boolean = false): Card[] {
+      const newDeck: Card[] = []
 
-    for (const suit of SUITS) {
-      for (const rank of RANKS) {
-        newDeck.push(new Card(suit, rank))
+      for (const suit of SUITS) {
+        for (const rank of RANKS) {
+          newDeck.push(new Card(suit, rank))
+        }
+      }
+
+      if (addJoker) {
+        newDeck.push(Card.createJoker())
+        newDeck.push(Card.createJoker())
+      }
+      return newDeck
+    }
+
+    // 呼び出しに使われるメソッド 公開
+    public shuffle(): void {
+      Deck.performShuffle(this.deck)
+    }
+
+    // 実際にシャッフルを行うメソッド 非公開・静的
+    private static performShuffle(deck: Card[]): void {
+      for (let i = deck.length - 1; i >= 0; i--) {
+        const j: number = Math.floor(Math.random() * (i + 1))
+        ;[deck[i], deck[j]] = [deck[j], deck[i]]
       }
     }
 
-    if (addJoker) {
-      newDeck.push(new Card(undefined, "Joker"))
+    public resetDeck(): void {
+      this.deck = Deck.generateDeck(GAMESWITHJOKER.includes(this.gameType))
     }
-    if (shuffle) {
-      Deck.shuffle(newDeck)
-    }
-    return newDeck
-  }
 
-  private static shuffle(deck: Card[]): void {
-    for (let i = deck.length - 1; i >= 0; i--) {
-      const j: number = Math.floor(Math.random() * (i + 1))
-      ;[deck[i], deck[j]] = [deck[j], deck[i]]
+    // undefinedの場合は呼び出し元で管理する
+    public drawOne(): Card | undefined {
+      return this.deck.shift()
     }
   }
-
-  public resetDeck(): void {
-    this.deck = Deck.generateDeck(GAMESWITHJOKER.includes(this.gameType))
-  }
-
-  // 先頭から一枚取り出す
-  public drawOne(): Card | undefined {
-    // TODO: ゲームごとに実装がことなってくるかも
-    if (this.deck.length === 0) {
-      throw new Error("Deck is empty")
-    }
-    return this.deck.shift()
-  }
-}
