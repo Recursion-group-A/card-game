d613441bb5370498c6e39866351cc161cb1597ce
Naoto Nishimura <naoto.n.2460@gmail.com>
2023-11-17 17:54:40 +0300

    change

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const STARTPAGE = document.getElementById("startPage")
+export const SETTINGPAGE = document.getElementById("settingPage")
+export const GAMEPAGE = document.getElementById("gamePage")
+
---
+++ src/views/gamePage.ts
@@ -0,0 +1,2 @@
+import { GAMEPAGE } from "@/config";
+
