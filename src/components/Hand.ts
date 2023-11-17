6f09635adac30db22449fed508060e557910c62f
Kyo Terada <sitiang120@gmail.com>
2023-11-17 21:25:29 +0900

    ♻️ Handクラスのリファクタリング

--- src/components/Hand.ts
+++ src/components/Hand.ts
@@ -1,12 +1,13 @@
 import { Card } from "@/components/Card"
 
-export class Hand {
+export default class Hand {
   private hand: Card[]
 
   constructor() {
     this.hand = []
   }
 
+  // 外部で変更されないようにコピーを返す
   public getHand(): Card[] {
     return [...this.hand]
   }
@@ -15,13 +16,9 @@ export class Hand {
     this.hand.push(card)
   }
 
+  // 呼び出し元でハンドリングを行う
   public pickOne(): Card | undefined {
     return this.hand.shift()
-  }
-
-  // TODO: 必要ないかも
-  public replaceHand(cards: Card[]): void {
-    this.hand = [...cards]
   }
 
   public cleanHand(): void {
@@ -30,5 +27,29 @@ export class Hand {
 
   public getCardCount(): number {
     return this.hand.length
+  }
+
+  // TODO:ブラックジャックに特化　あとで変える
+  public getHandTotalScore(): number {
+    let total = 0
+    let aceCount = 0
+
+    for (const card of this.hand) {
+      if (card.getRank() === "A") {
+        aceCount++
+      }
+      total += card.getRankNumber()
+    }
+
+    while (total > 21 && aceCount > 0) {
+      total -= 10
+      aceCount--
+    }
+
+    return total
+  }
+
+  public isBlackjack(): boolean {
+    return this.getHandTotalScore() === 21 && this.getCardCount() === 2
   }
 }
