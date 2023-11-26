df51e48aa3c0f53791e9533f205ec0e3e02e6c65
Naoto Nishimura <naoto.n.2460@gmail.com>
2023-11-26 19:41:47 +0300

    actionView.tsxの作成

--- src/app/Blackjack/gamePage/page.tsx
+++ src/app/Blackjack/gamePage/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ActionVew from './actionView'
 
 export default function createBlackjackMainPage() {
   return (
@@ -157,46 +158,7 @@ export default function createBlackjackMainPage() {
                 </div>
               </div>
             </div>
-            <div id="userAction">
-              <div className="flex items-center justify-around">
-                <div className="m-3 flex flex-col items-center justify-center">
-                  <button
-                    type="button"
-                    aria-label="SURRENDER"
-                    id="surrenderBtn"
-                    className="h-[4.5rem] w-[4.5rem]  rounded-full bg-red-600 text-lg font-semibold text-white hover:bg-blue-500 hover:opacity-60"
-                  />
-                  <p className="pt-2 text-center font-bold">SURRENDER</p>
-                </div>
-                <div className="m-3 flex flex-col items-center justify-center">
-                  <button
-                    type="button"
-                    aria-label="STAND"
-                    id="standBtn"
-                    className="h-[4.5rem] w-[4.5rem]  rounded-full bg-green-700 text-lg font-semibold text-white hover:bg-green-200 hover:opacity-60"
-                  />
-                  <p className="pt-2 text-center font-bold">STAND</p>
-                </div>
-                <div className="m-3 flex flex-col items-center justify-center">
-                  <button
-                    type="button"
-                    aria-label="HIT"
-                    id="hitBtn"
-                    className="h-[4.5rem] w-[4.5rem]  rounded-full bg-yellow-300 text-lg font-semibold text-white hover:bg-yellow-400 hover:opacity-60"
-                  />
-                  <p className="pt-2 text-center font-bold">HIT</p>
-                </div>
-                <div className="m-3 flex flex-col items-center justify-center">
-                  <button
-                    type="button"
-                    aria-label="DOUBLE"
-                    id="doubleBtn"
-                    className="h-[4.5rem] w-[4.5rem]  rounded-full bg-blue-500 text-lg font-semibold text-white hover:bg-blue-400 hover:opacity-60"
-                  />
-                  <p className="pt-2 text-center font-bold">DOUBLE</p>
-                </div>
-              </div>
-            </div>
+            <div id="userAction">{ActionVew.render()}</div>
           </div>
           <div id="CP2" className="flex w-1/4 flex-col items-center">
             <p className="text-center text-3xl text-yellow-200">CP2</p>
