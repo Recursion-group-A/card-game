5ff00be5febe3dcf1bef0897e305ce2c0cea0f0e
Kyo Terada <sitiang120@gmail.com>
2023-12-16 21:47:58 +0900

    utilファイルのコミット

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,8 +4,13 @@ import BlackjackRankStrategy from '@/models/blackjack/BlackjackRankStrategy'
 import PokerRankStrategy from '@/models/poker/PokerRankStrategy'
 
 // TODO: Speed も追加する
-export default function getRankStrategy(gameType: GAMETYPE): RankStrategy {
+export function getRankStrategy(gameType: GAMETYPE): RankStrategy {
   return gameType === GAMETYPE.Blackjack
     ? new BlackjackRankStrategy()
     : new PokerRankStrategy()
+}
+
+export function delay(ms: number): Promise<void> {
+  // eslint-disable-next-line
+  return new Promise<void>((resolve) => setTimeout(resolve, ms))
 }
