bfc1274c90a401ed7316149fb109bffbfd34d1fc
Kyo Terada <sitiang120@gmail.com>
2023-12-19 01:51:51 +0900

    🔥 不要ファイルの削除

--- src/phaser/common/BetView.ts
+++
@@ -1,7 +0,0 @@
-// import Phaser from 'phaser'
-//
-// export default class BetView extends Phaser.GameObjects.Container {
-//   constructor(scene: Phaser.Scene, x: number, y: number) {
-//     super(scene, x, y)
-//   }
-// }
